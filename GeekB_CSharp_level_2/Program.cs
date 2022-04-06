09ca9f7b679010432286cb131cfa9b552b11a368
Askhab <asmatsagov@gmail.com>
2022-04-06 17:10:57 +0500

    Add project files.

---
+++ GeekB_CSharp_level_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekB_CSharp_level_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's start!");
+            Console.ReadKey();
+        }
+    }
+}
