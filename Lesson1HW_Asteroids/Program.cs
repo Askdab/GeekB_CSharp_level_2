54fc6ad563e19bab3240f3167fc0cabc829b6299
Askhab <asmatsagov@gmail.com>
2022-04-11 17:34:15 +0500

    Start HomeWork
    Added project asteroids and class game

---
+++ Lesson1HW_Asteroids/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson1HW_Asteroids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            form.Show();
+            Application.Run(form);
+        }
+    }
+}
