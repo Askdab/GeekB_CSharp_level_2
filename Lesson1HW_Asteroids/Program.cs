71bb23a6624ab454ad6ca0acb47ac414d44ee07e
Askhab <asmatsagov@gmail.com>
2022-04-12 07:14:38 +0500

    Added dinamic Points

--- Lesson1HW_Asteroids/Program.cs
+++ Lesson1HW_Asteroids/Program.cs
@@ -14,7 +14,9 @@ namespace Lesson1HW_Asteroids
             Form form = new Form();
             form.Width = 800;
             form.Height = 600;
+            Game.Init(form);
             form.Show();
+            Game.Draw();
             Application.Run(form);
         }
     }
